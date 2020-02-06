8ddd9649907463a830ca5c9baf12e98b221342e3
Vinyljam <hubertstrawa@gmail.com>
2020-02-06 21:03:39 +0100

    adding some changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 let links = document.querySelectorAll('.menu > div > div > ul > li > a');
-    let arr = Array.from(links);
-    arr.forEach(i => {
-        i.addEventListener('click', () => {
-            document.querySelector('.toggler').checked = false;
-        });
-    })
+let arr = Array.from(links);
+arr.forEach(i => {
+    i.addEventListener('click', () => {
+        document.querySelector('.toggler').checked = false;
+    });
+})
+
+const btn = document.querySelector('.button_more_btn');
+const morePhotos = document.getElementById('seeMorePhotos');
+
+btn.addEventListener('click', function() {
+    morePhotos.style.display = 'inline-flex';
+    btn.style.display = 'none';
+})
