649a835711b6497b29c25c9c5785c107f7d6bac7
hubertstrawa <hubertstrawa@gmail.com>
2021-06-30 10:55:31 +0200

    Adding some changes

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,28 @@ btn.addEventListener('click', function() {
     morePhotos.style.display = 'inline-flex';
     btn.style.display = 'none';
 })
+
+
+$(document).ready(function(){
+  $('.accordion_item > h3').on('click',function(){
+      $('.accordion_item > .box-accordion').each(function(k,v){
+        $(v).css('display','none');
+        $(v).prev().children().removeClass('turned');
+      })
+      $(this).next().fadeToggle()
+      $(this).children().addClass('turned')
+  })
+  $('.regulamin-btn-expand').on('click', function(){
+    $(this).css({opacity: '0', visibility: 'hidden'})
+    $('.regulamin-btn-less').css({opacity: '1', visibility: 'unset'})
+    $('.regulamin-text-expanded').fadeIn()
+  })
+  $('.regulamin-btn-less').on('click', function(){
+    $(this).css({opacity: '0', visibility: 'hidden'})
+    $('.regulamin-btn-expand').css({opacity: '1', visibility: 'unset'})
+    $('.regulamin-text-expanded').fadeOut()
+  })
+})
+
+
+
